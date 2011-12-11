a0210c6d20b54d9db62f5419d54d9ce713829ad8
Rune Kaagaard <rumi.kg@gmail.com>
2011-12-11 11:32:30 +0100

    Fixing tryme.

--- test_proj/tryme.sh
+++ test_proj/tryme.sh
@@ -1,5 +1,5 @@
 git clone git@github.com:runekaagaard/django-locking.git
 cd django-locking/test_proj
 python manage.py syncdb
-echo "Try out django-locking at: http://localhost/admin:8000".
-python runserver
+echo "Try out django-locking at: http://127.0.0.1:8000/admin".
+python manage.py runserver
