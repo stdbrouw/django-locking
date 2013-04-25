3f6fb094f931a3aa2df6a66e95f4107522fb5b15
Frankie Dintino <fdintino@theatlantic.com>
2013-04-25 15:11:56 -0400

    Increment version of admin.locking.js

--- locking/admin.py
+++ locking/admin.py
@@ -22,7 +22,7 @@ class LockableAdmin(admin.ModelAdmin):
         js = (
             _s.STATIC_URL + 'locking/js/jquery.url.packed.js',
             _s.LOCKING_URL + "ajax/variables.js",
-            _s.STATIC_URL + "locking/js/admin.locking.js?v=1"
+            _s.STATIC_URL + "locking/js/admin.locking.js?v=5"
         )
         css = {"all": (_s.STATIC_URL + 'locking/css/locking.css',)}
 
